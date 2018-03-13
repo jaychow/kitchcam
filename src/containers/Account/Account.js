942ff23c336e5b907ff036ed0302dd38aee7fcfe
patrick <patimpey@gmail.com>
2018-03-12 18:50:14 -0700

    updates for account page

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -7,12 +7,19 @@ import Helmet from 'react-helmet';
 export default class Account extends Component {
     render() {
         const styles = require('./Account.scss');
+        const spices = require('./spices.jpg');
+        const patrick = require('./patrick.jpg');
 
         return (
-            <div className={styles.home + ' container-fluid'}>
+            <div className={styles.account + ' container-fluid'}>
                 <Helmet title="Account Page"/>
                 <div className="col-xs-12">
-                    <h1>Account</h1><button className="btn btn-default">Start Stream</button>
+                    <h1 className={styles.accountHeader}>
+                        <img className={styles.accountPhoto}
+                             height="300" data-toggle="tooltip" title="Spices!" src={patrick}/>Account
+                    </h1>
+                    <button className={`${styles.streamButton} ${styles.raise} btn btn-default`}>
+                        <i className="fa fa-video-camera"/> Start Stream</button>
                 </div>
                 <div className="row">
                     <div className="col-xs-4">
@@ -22,18 +29,8 @@ export default class Account extends Component {
                         2 of 3 (wider)
                     </div>
                     <div className="col-xs-4">
-                        3 of 3
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-4">
-                        1 of 3
-                    </div>
-                    <div className="col-xs-4">
-                        2 of 3 (wider)
-                    </div>
-                    <div className="col-xs-4">
-                        3 of 3
+                        <img className={styles.spices}
+                             height="300" data-toggle="tooltip" title="Spices!" src={spices}/>
                     </div>
                 </div>
             </div>
